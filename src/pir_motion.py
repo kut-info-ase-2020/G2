005fafe78c28c5bcedac5b652b8f1e68616c01bc
YasuyukiTsutsui <245127p@gs.kochi-tech.ac.jp>
2020-07-15 16:00:53 +0900

    add setup main destroy

--- src/pir_motion.py
+++ src/pir_motion.py
@@ -5,16 +5,29 @@
 import RPi.GPIO as GPIO
 
 PIR_input = 17				#read PIR Output
-LED = 18				#LED for signalling motion detected	
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)		#choose pin no. system
-GPIO.setup(PIR_input, GPIO.IN)	
-GPIO.setup(LED, GPIO.OUT)
-GPIO.output(LED, GPIO.LOW)
+LED = 18
+				#LED for signalling motion detected	
+def setup():
+	GPIO.setwarnings(False)
+	GPIO.setmode(GPIO.BOARD)		#choose pin no. system
+	GPIO.setup(PIR_input, GPIO.IN)	
+	GPIO.setup(LED, GPIO.OUT)
+	GPIO.output(LED, GPIO.LOW)
 
-while True:
-#when motion detected turn on LED
-    if(GPIO.input(PIR_input)):
-        GPIO.output(LED, GPIO.HIGH)
-    else:
-        GPIO.output(LED, GPIO.LOW)
+def main():
+	while True:
+		#when motion detected turn on LED
+    		if(GPIO.input(PIR_input)):
+        		GPIO.output(LED, GPIO.HIGH)
+    		else:
+        		GPIO.output(LED, GPIO.LOW)
+
+def destroy():
+    GPIO.cleanup()
+
+if __name__ == '__main__':
+    setup()
+    try:
+        main()
+    except KeyboardInterrupt:
+        destroy() 

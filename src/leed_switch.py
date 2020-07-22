ca8785db6b93b471113d5c5615c6a04ccfcb6b31
IH-kt <200283r@ugs.kochi-tech.ac.jp>
2020-07-22 16:56:51 +0900

    add led

--- src/leed_switch.py
+++ src/leed_switch.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 leed_input = 26
+LED = 18
 
 def setup():
     GPIO.setwarnings(False)
@@ -9,8 +10,8 @@ def setup():
     #GPIO.setwarnings(False)
     #GPIO.setmode(GPIO.BOARD)		#choose pin no. system
     GPIO.setup(leed_input, GPIO.IN)	
-    # GPIO.setup(LED, GPIO.OUT)
-    # GPIO.output(LED, GPIO.LOW)
+    GPIO.setup(LED, GPIO.OUT)
+    GPIO.output(LED, GPIO.LOW)
 
 def main():
     while True:
@@ -18,10 +19,10 @@ def main():
         #when motion detected turn on LED
         input = GPIO.input(leed_input)
         print(input)
-        # if(input):
-        # 	GPIO.output(LED, GPIO.HIGH)
-        # else:
-        # 	GPIO.output(LED, GPIO.LOW)
+        if(!input):
+            GPIO.output(LED, GPIO.HIGH)
+        else:
+            GPIO.output(LED, GPIO.LOW)
 
 def destroy():
     GPIO.cleanup()

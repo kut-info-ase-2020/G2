e58f76d54b88734f33703e85aa8ea0a301c48176
IH-kt <200283r@ugs.kochi-tech.ac.jp>
2020-07-22 14:58:45 +0900

    minor change

--- src/leed_switch.py
+++ src/leed_switch.py
@@ -4,24 +4,24 @@ leed_input = 26
 
 def setup():
     GPIO.setwarnings(False)
-        #set the gpio modes to BCM numbering
-        GPIO.setmode(GPIO.BCM)
-        #GPIO.setwarnings(False)
-        #GPIO.setmode(GPIO.BOARD)		#choose pin no. system
-        GPIO.setup(leed_input, GPIO.IN)	
-        # GPIO.setup(LED, GPIO.OUT)
-        # GPIO.output(LED, GPIO.LOW)
+    #set the gpio modes to BCM numbering
+    GPIO.setmode(GPIO.BCM)
+    #GPIO.setwarnings(False)
+    #GPIO.setmode(GPIO.BOARD)		#choose pin no. system
+    GPIO.setup(leed_input, GPIO.IN)	
+    # GPIO.setup(LED, GPIO.OUT)
+    # GPIO.output(LED, GPIO.LOW)
 
 def main():
     while True:
         time.sleep(1)
-                #when motion detected turn on LED
-                input = GPIO.input(leed_input)
-                print(input)
-                # if(input):
-                # 	GPIO.output(LED, GPIO.HIGH)
-                # else:
-                # 	GPIO.output(LED, GPIO.LOW)
+        #when motion detected turn on LED
+        input = GPIO.input(leed_input)
+        print(input)
+        # if(input):
+        # 	GPIO.output(LED, GPIO.HIGH)
+        # else:
+        # 	GPIO.output(LED, GPIO.LOW)
 
 def destroy():
     GPIO.cleanup()

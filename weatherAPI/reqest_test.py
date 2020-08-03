9347a2d8ec34e355c93551479fca21e2344debcd
YasuyukiTsutsui <245127p@gs.kochi-tech.ac.jp>
2020-08-03 14:57:01 +0900

    mod request_test

--- weatherAPI/reqest_test.py
+++ weatherAPI/reqest_test.py
@@ -1,13 +1,13 @@
 import requests
 import requests
 r = requests.get("http://abehiroshi.la.coocan.jp/")
-#view text data
+#view encoded text data
 print(r.text.encode('utf-8'))
 #view http connection status
-print(r.status_code)
+print(r.status_code, type(r.status_code))
 #view data header
 print(r.headers)
 #view data character code
 print(r.encoding)
-#view encoded text data
+#view unencoded text data
 print(r.content)

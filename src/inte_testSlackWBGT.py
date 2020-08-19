73774464895768d7208e6bb7d3386722943e5776
YasuyukiTsutsui <245127p@gs.kochi-tech.ac.jp>
2020-08-19 14:41:45 +0900

    mod class name

--- src/inte_testSlackWBGT.py
+++ src/inte_testSlackWBGT.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
         print("危なかったら１、危なくなかったら０、故障していたらー１が返ってくるはずです!")
         print("データは「dataWBGT.csv」に入っています!")
         print(calWBGT.calWBGT())
-        api = SlackAPI_class.SlackAPI(
+        api = SlackAPI(
             token=os.environ['SLACK_API_TOKEN'], 
             channels = '#zikkenzyou_go'
             )

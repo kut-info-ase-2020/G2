7b8f0b551cfac90f237c46269ec0699ee0c05017
skinny_man <32474866+GoOhira@users.noreply.github.com>
2020-09-28 18:19:41 +0900

    C:/Program Files/Git/gitignore added

--- src/SlackAPI/SlackAPI_class.py
+++ src/SlackAPI/SlackAPI_class.py
@@ -58,7 +58,7 @@ class SlackAPI:
     def Notification_HeatStroke(self,Temp,Hum):
         try:
             print("Heatstroke notification message create...")
-            message = "Heat stroke is a danger! Open a window, turn on the air conditioner, etc.!\nTemprerature is "+ str(Temp) + ".\nHumidity is " + str(Hum) + "."
+            message = "Warning: You have a high risk of heat stroke! You should open a window…\nTemprerature is "+ str(Temp) + ".\nHumidity is " + str(Hum) + "."
             self.Notification_send(message)
         except IndexError:
             print("Error! Bad argument.")

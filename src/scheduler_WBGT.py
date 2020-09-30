37d96c25aeddf5b3ce8cf43d430a1d47562589cc
YasuyukiTsutsui <245127p@gs.kochi-tech.ac.jp>
2020-09-30 14:00:29 +0900

    use calWBGT class

--- src/scheduler_WBGT.py
+++ src/scheduler_WBGT.py
@@ -1,4 +1,4 @@
-import calWBGT
+from calWBGT import calWBGT
 from SlackAPI.SlackAPI_class import SlackAPI
 from sched import scheduler
 import os
@@ -10,6 +10,7 @@ from datetime import date
 import csv
 
 def timer_loop():
+    cal_wbgt = calWBGT()
     timer_sched = scheduler(time, sleep)
     current = datetime.now()
     # if current.minute < 30:
@@ -25,26 +26,28 @@ def timer_loop():
             writer = csv.writer(f)
             writer.writerow(['date','Hum','Temp','WBGT'])
 
+    interval = 15 #setting demo interval
     while True:
         timer_sched.enterabs(
             next_time.timestamp(), 1, run_calWBGT,
-            kwargs={'next_time': next_time,
-                    'today': today,
+            kwargs={'today': today,
                     'current': current,
-                    'csv_path': export_csv_name,})
+                    'csv_path': export_csv_name,
+                    'interval': interval,
+                    'cal_wbgt': cal_wbgt,})
         timer_sched.run()
         if today < date.today():
-        #if current + timedelta(minutes=10) < datetime.now():
+        #if current + timedelta(minutes=interval) < datetime.now():
             current = datetime.now()
             current = current.replace(microsecond=0) # for test
             today = date.today()
             export_csv_name = "data/heatstroke-" + str(today.year) + "-" + str(today.month) + "-" + str(today.day) + ".csv"
             
 
-        next_time = next_time + timedelta(minutes=10)
+        next_time = next_time + timedelta(minutes=interval)
         # next_time = next_time + timedelta(minutes=1)
 
-def run_calWBGT(next_time, today, current, csv_path):
+def run_calWBGT(today, current, csv_path, interval, cal_wbgt):
     if today < date.today():
     # if current + timedelta(minutes=10) < datetime.now():
         api = SlackAPI(
@@ -56,7 +59,7 @@ def run_calWBGT(next_time, today, current, csv_path):
         with open(csv_path, 'w') as f:
             writer = csv.writer(f)
             writer.writerow(['date','Hum','Temp','WBGT'])
-    calWBGT.calWBGT(csv_path)
+    cal_wbgt.calWBGT(csv_path)
 
 
 if __name__=='__main__':
